ac4e9507614d7736965434d0edb36c5e1791d03e
BlendBoy <BlendBoy@ee5284b2-0c00-11de-85a1-fd18384f7567>
2009-05-31 04:31:26 +0000

    Agregadas las clases fuente y tests unitarios de JHardmin.

--- proyectos/JHard/src/java/edu/ues/jhard/beans/BeanBaseJHardmin.java
+++ proyectos/JHard/src/java/edu/ues/jhard/beans/BeanBaseJHardmin.java
@@ -5,10 +5,30 @@
 
 package edu.ues.jhard.beans;
 
+import edu.ues.jhard.jpa.Usuario;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author Hugol
  */
-public class BeanBaseJHardmin {
+public class BeanBaseJHardmin extends BeanBase {
 
+    public Usuario getUsuario(String userName, String userPwd){
+        Usuario u = null;
+        try{
+            System.out.println("Creando el entity manager...");
+            EntityManager eMgr = this.getEntityManager();
+            System.out.println("Entity manager creado. Generando el query...");
+            String strSql = "SELECT u FROM usuario u WHERE u.nombre ='" + userName + "' and u.clave='" + userPwd + "'";
+            System.out.println("String sql generado: " + strSql);
+            u = (Usuario)eMgr.createQuery(strSql).getSingleResult();
+            System.out.println("Usuario creado.");
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        System.out.println("Usuario devuelto: " + u);
+        return u;
+    }
 }
