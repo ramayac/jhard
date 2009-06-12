9464888344ed775c5733327b52971611cb6d2405
hugol.barrientos <hugol.barrientos@ee5284b2-0c00-11de-85a1-fd18384f7567>
2009-06-12 06:40:33 +0000

    cambios en BeanBaseJRequest y EquipoSimple

--- proyectos/JHard/src/java/edu/ues/jhard/jpa/Equiposimple.java
+++ proyectos/JHard/src/java/edu/ues/jhard/jpa/Equiposimple.java
@@ -27,7 +27,9 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "equiposimple", catalog = "jhard", schema = "")
-@NamedQueries({@NamedQuery(name = "Equiposimple.findAll", query = "SELECT e FROM Equiposimple e"), @NamedQuery(name = "Equiposimple.findByIdEquipoSimple", query = "SELECT e FROM Equiposimple e WHERE e.idEquipoSimple = :idEquipoSimple"), @NamedQuery(name = "Equiposimple.findByPropietario", query = "SELECT e FROM Equiposimple e WHERE e.propietario = :propietario")})
+@NamedQueries({@NamedQuery(name = "Equiposimple.findAll", query = "SELECT e FROM Equiposimple e"), 
+               @NamedQuery(name = "Equiposimple.findByIdEquipoSimple", query = "SELECT e FROM Equiposimple e WHERE e.idEquipoSimple = :idEquipoSimple"),
+               @NamedQuery(name = "Equiposimple.findByPropietario", query = "SELECT e FROM Equiposimple e WHERE e.propietario = :propietario")})
 public class Equiposimple implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
