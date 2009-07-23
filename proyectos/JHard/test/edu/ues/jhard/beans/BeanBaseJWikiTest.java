99008127396928e6b00c4a42979058c9d6089544
Ramayac <Ramayac@ee5284b2-0c00-11de-85a1-fd18384f7567>
2009-07-23 23:07:43 +0000

    Commit por sanidad mental: Se añaden... 	BeanBaseJWiki 	Unit Test de JWiki 	se añade la clase Articulos a la persistencia.
    More functionality to come.. :)

---
+++ proyectos/JHard/test/edu/ues/jhard/beans/BeanBaseJWikiTest.java
@@ -0,0 +1,115 @@
+package edu.ues.jhard.beans;
+
+import edu.ues.jhard.jpa.Articulos;
+import edu.ues.jhard.jpa.Usuario;
+import java.sql.Date;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ramayac
+ */
+public class BeanBaseJWikiTest {
+
+    public BeanBaseJWikiTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Obtenemos una articulo de la BD
+     */
+    @Test
+    public void testGetArticulos() {
+        System.out.println("getArticulos");
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        //Articulos expResult = null;
+        Articulos result = instance.getArticulo(1);
+        assertNotNull(result);
+        //assertEquals(expResult, result);
+        System.out.println("Exito en la prueba!");
+    }
+
+    /**
+     * Obtejemos N articulos de la BD
+     */
+    @Test
+    public void testNArticulos() {
+        int n = 2;
+        //System.out.println("testNArticuloss");
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        List<Articulos> resultado = instance.getUltimosNArticulos(n);
+        if(resultado.size()==n)
+            assertNotNull(resultado.get(0)); //por lo menos, que la primera NO sea null
+        else
+            fail("resultado menor que el esperado ("+n+")");
+    }
+
+    @Test
+    public void testgetAllArticulos() {
+        //System.out.println("testNArticuloss");
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        List<Articulos> resultado = instance.getAllArticulos();
+        assertNotNull(resultado);
+    }
+
+    /**
+     * Agregamos una Articulos a la BD
+     */
+    @Test
+    public void testRegistrarArticulos() {
+        //System.out.println("testRegistrarArticulos");
+        BeanBaseJHardmin hardmin = new BeanBaseJHardmin();
+        Usuario usuario = hardmin.getUsuario(1);
+        Articulos articulo = new Articulos(0, "El articulo 9999", "nain, nine, nueve, 9, iiiiiiii, etc etc etc", new Date(2009, 9, 9), usuario.getIdusuario());
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        if(!instance.createArticulo(articulo)) fail("fallo en REGISTRAR ENTRADA");
+    }
+
+    /**
+     * Busqueda por titulo de articulo
+     */
+    @Test
+    public void testSearchArticulosPorTitulo() {
+        String criteria = "9999";
+        //System.out.println("testSearchArticulosPorTitulo");
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        List<Articulos> la = (List<Articulos>)instance.searchArticuloPorTitulo(criteria);
+        assertNotNull(la);
+    }
+
+    /**
+     * Elimina una articulo
+     */
+    @Test
+    public void testEliminarArticulos() {
+        //System.out.println("testEliminarArticulos");
+        String criteria = "9999";
+        BeanBaseJWiki instance = new BeanBaseJWiki();
+        Articulos articulo = instance.searchArticuloPorTitulo(criteria).get(0);
+        instance.deleteArticulo(articulo);
+        List<Articulos> la= instance.searchArticuloPorTitulo(criteria);
+        if(la.size()!=0) fail("fallo en ELIMINAR ENTRADA");
+    }
+
+}
