164e500cf56ba9eccf7c53758a980c0792458042
Ramayac <Ramayac@ee5284b2-0c00-11de-85a1-fd18384f7567>
2009-08-09 17:50:01 +0000

    Se QUITA las tareas comunes de: jmlCrudAdmin, jmlAsistencia, jwikiUser, jmlInscripcion, jmlGestionaClase, jwikiAdmin y jmlLaboratorio, y se sustituye por el "control": tareas.jspx para que se incluya como el menu.jspx y login.jspx. Se añade jmlCrudAdmin, sin contenido aun.

---
+++ trunk/proyectos/JHard/src/java/jhard/jmlCrudAdmin.java
@@ -0,0 +1,117 @@
+/*
+ * jmlCrudAdmin.java
+ *
+ * Created on 09-9-2009, 10:56:59 AM
+ * Copyright ramayac
+ */
+package jhard;
+
+import edu.ues.jhard.beans.BeanBaseJManLab;
+import javax.faces.FacesException;
+
+/**
+ * <p>Page bean that corresponds to a similarly named JSP page.  This
+ * class contains component definitions (and initialization code) for
+ * all components that you have defined on this page, as well as
+ * lifecycle methods and event handlers where you may add behavior
+ * to respond to incoming events.</p>
+ */
+public class jmlCrudAdmin extends BeanBaseJHard {
+    
+    private int __placeholder;
+
+    private BeanBaseJManLab jml = new BeanBaseJManLab();
+
+    /**
+     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
+     * This method is automatically generated, so any user-specified code inserted
+     * here is subject to being replaced.</p>
+     */
+    private void _init() throws Exception {
+    }
+
+    public  BeanBaseJManLab getJManLabInstance() {
+        return this.jml;
+    }
+
+    /**
+     * <p>Construct a new Page bean instance.</p>
+     */
+    public jmlCrudAdmin() {
+
+    }
+
+    /**
+     * Metodo para obtener una entrada por su ID
+     * @param identrada id de la entada que se desea
+     * @return
+     */
+//    public String elegirArticuloActual() {
+//        String idSeleccionado = ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("idSeleccionado");
+//        Integer id = new Integer(idSeleccionado);
+//        this.articuloActual = this.jwikiInstance.getArticulo(id.intValue());
+//        this.setSoloUna(true);
+//        return EMPTY_STRING;
+//    }
+
+    /**
+     * <p>Callback method that is called whenever a page is navigated to,
+     * either directly via a URL, or indirectly via page navigation.
+     * Customize this method to acquire resources that will be needed
+     * for event handlers and lifecycle methods, whether or not this
+     * page is performing post back processing.</p>
+     *
+     * <p>Note that, if the current request is a postback, the property
+     * values of the components do <strong>not</strong> represent any
+     * values submitted with this request.  Instead, they represent the
+     * property values that were saved for this view when it was rendered.</p>
+     */
+    public void init() {
+        // Perform initializations inherited from our superclass
+        super.init();
+// Perform application initialization that must complete
+        // *before* managed components are initialized
+        // TODO - add your own initialiation code here
+// <editor-fold defaultstate="collapsed" desc="Visual-Web-managed Component Initialization">
+// Initialize automatically managed components
+        // *Note* - this logic should NOT be modified
+        try {
+            _init();
+        } catch (Exception e) {
+            log("Page1 Initialization Failure", e);
+            throw e instanceof FacesException ? (FacesException) e : new FacesException(e);
+        }
+    }
+
+    /**
+     * <p>Callback method that is called after the component tree has been
+     * restored, but before any event processing takes place.  This method
+     * will <strong>only</strong> be called on a postback request that
+     * is processing a form submit.  Customize this method to allocate
+     * resources that will be required in your event handlers.</p>
+     */
+    public void preprocess() {
+    }
+
+    /**
+     * <p>Callback method that is called just before rendering takes place.
+     * This method will <strong>only</strong> be called for the page that
+     * will actually be rendered (and not, for example, on a page that
+     * handled a postback and then navigated to a different page).  Customize
+     * this method to allocate resources that will be required for rendering
+     * this page.</p>
+     */
+    public void prerender() {
+    }
+
+    /**
+     * <p>Callback method that is called after rendering is completed for
+     * this request, if <code>init()</code> was called (regardless of whether
+     * or not this was the page that was actually rendered).  Customize this
+     * method to release resources acquired in the <code>init()</code>,
+     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
+     * acquired during execution of an event handler).</p>
+     */
+    public void destroy() {
+    }
+}
---
+++ trunk/proyectos/JHard/web/jmlCrudAdmin.jsp
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- 
+    Document   : jmlCrudAdmin
+    Created on : 06-21-2009, 6:00:43 AM
+    Author     : ramayac
+-->
+<jsp:root version="2.1" xmlns:f="http://java.sun.com/jsf/core" xmlns:h="http://java.sun.com/jsf/html" xmlns:ice="http://www.icesoft.com/icefaces/component" xmlns:jsp="http://java.sun.com/JSP/Page">
+    <jsp:directive.page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"/>
+    <f:view>
+        <html id="outputHtml1">
+            <head id="outputHead1">
+                <ice:outputStyle href="css/stylesheet.css" id="outputStyle1"/>
+                <ice:outputStyle href="./xmlhttp/css/rime/rime.css" id="outputStyle2"/>
+                <meta content="text/html; charset=utf-8" http-equiv="content-type"/>
+                <title>Administración del Laboratorio</title>
+                <meta content="" name="keywords"/>
+                <meta content="" name="description"/>
+                <link href="css/default.css" rel="stylesheet" type="text/css"/>
+                <link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+            </head>
+            <body id="outputBody1" style="-rave-layout: grid">
+                <!--start header -->
+                <jsp:directive.include file="/jspf/menu.jspx"/>
+                <!-- end header -->
+                <!-- start page -->
+                <div id="page">
+                    <!-- start content -->
+                    <div id="content">
+                        <div class="post">
+                            <h2 class="title">Administración del Laboratorio</h2>
+                        </div>
+                        <ice:form id="formAdmLab">
+                        </ice:form>
+                        <br/>
+                    </div>
+                    <!-- end content -->
+                    <!-- start sidebar -->
+                    <div id="sidebar">
+                        <ul>
+                            <li>
+                            <!--login control -->
+                            <jsp:directive.include file="/jspf/login.jspx"/>
+                            <!--login control -->
+                            </li>
+                            <li>
+                            <!--login control -->
+                            <jsp:directive.include file="/jspf/tareas.jspx"/>
+                            <!--login control -->
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- end sidebar -->
+                    <div style="clear: both;"></div>
+                </div>
+                <!-- end page -->
+                <!-- start footer -->
+                <jsp:directive.include file="/jspf/bottom.jspx"/>
+                <!-- end footer -->
+            </body>
+        </html>
+    </f:view>
+</jsp:root>
